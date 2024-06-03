25504e3a40fc96b7e14ae0a9c01721acf8d56b1d
Evgeny Bayandin <5087081@gmail.com>
2024-06-03 11:45:15 +0300

    Сreated package controller? added PersonWithBookController.java

---
+++ person-with-book-microservice/src/main/java/ru/webdev/personwithbookmicroservice/controller/PersonWithBookController.java
@@ -0,0 +1,27 @@
+package ru.webdev.personwithbookmicroservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.webdev.personwithbookmicroservice.model.Book;
+import ru.webdev.personwithbookmicroservice.model.Person;
+
+@RestController
+@RequestMapping("/reader")
+public class PersonWithBookController {
+
+    @Autowired
+    public org.springframework.web.client.RestTemplate restTemplate;
+
+    @GetMapping("/{personId}/{bookId}")
+    public ResponseEntity<Person> reader(@PathVariable int personId, @PathVariable int bookId) {
+        Person person = restTemplate.getForObject("http:\\localhost:8082/person/" + personId, Person.class);
+        Book book  = restTemplate.getForObject("http:\\localhost:8081/book/"  + bookId, Book.class);
+        person.setBook(book);
+        return new ResponseEntity<Person>(person, HttpStatus.OK);
+    }
+}
