3e2f5770c9a135f76f7ea72ac91ad36f36058ffc
Evgeny Bayandin <5087081@gmail.com>
2024-06-03 13:46:33 +0300

    Updated application.properties settings, added @EnableServerClient annotation in BookMicroserviceApplication.java, added server-eureka dependencies in pom.xml

---
+++ book-microservice/src/test/java/ru/webdev/bookmicroservice/BookMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.webdev.bookmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
