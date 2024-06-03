f7eb41f5ac4d885a2e1b8df5044d40d45815a6f2
Evgeny Bayandin <5087081@gmail.com>
2024-06-03 14:13:48 +0300

    Updated person-with-book-microservice pom.xml, person-microservice pom.xml, book-microservice pom.xml,  added @LoadBalansed in Config.java in person-with-book-microservice

--- person-with-book-microservice/src/main/java/ru/webdev/personwithbookmicroservice/config/Config.java
+++ person-with-book-microservice/src/main/java/ru/webdev/personwithbookmicroservice/config/Config.java
@@ -1,5 +1,6 @@
 package ru.webdev.personwithbookmicroservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ public class Config {
 
     // внедряем RestTemplate для работы с REST API и повторного использования в сервисах
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
