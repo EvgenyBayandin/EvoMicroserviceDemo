804404c4f3aa89d813012c413a8448fd3cd4819f
Evgeny Bayandin <5087081@gmail.com>
2024-06-03 13:49:41 +0300

    Updated module PersonWithBookMicroservice

---
+++ person-with-book-microservice/src/main/java/ru/webdev/personwithbookmicroservice/PersonWithBookMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ru.webdev.personwithbookmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonWithBookMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonWithBookMicroserviceApplication.class, args);
+    }
+
+}
