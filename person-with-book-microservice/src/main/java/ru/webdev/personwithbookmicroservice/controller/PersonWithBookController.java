2d96f7fcc2d97d329d884a1708ad05e1db5afaa3
Evgeny Bayandin <5087081@gmail.com>
2024-06-03 19:12:55 +0300

    Updated application.properties comments, Added @Value("${eureka.instance.instance-id}") and @GetMapping("/test")

--- person-with-book-microservice/src/main/java/ru/webdev/personwithbookmicroservice/controller/PersonWithBookController.java
+++ person-with-book-microservice/src/main/java/ru/webdev/personwithbookmicroservice/controller/PersonWithBookController.java
@@ -1,6 +1,7 @@
 package ru.webdev.personwithbookmicroservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +20,19 @@ public class PersonWithBookController {
     @Autowired
     public RestTemplate restTemplate;
 
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
     @GetMapping("/{personId}/{bookId}")
     public ResponseEntity<Person> reader(@PathVariable int personId, @PathVariable int bookId) {
         Person person = restTemplate.getForObject("http://person-microservice/person/" + personId, Person.class);
         Book book  = restTemplate.getForObject("http://book-microservice/book/"  + bookId, Book.class);
         person.setBook(book);
         return new ResponseEntity<Person>(person, HttpStatus.OK);
+    }
+
+    @GetMapping("/test")
+    public String test()  {
+        return "Hello from " + instanceId;
     }
 }
